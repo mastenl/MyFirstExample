e0a24c3ec785057c7fa6206b678dcc5932a302e1
mastenl <31227206+mastenl@users.noreply.github.com>
2020-02-05 11:40:43 -0800

    added stuff

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
+include <iostream>
 
 using std::cin;
 using std::cout;
 
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
-
+ int i;
+int j;   
+ cout<<"Hi, please enter two numbers: ";
+   cin >> i >> j;
     return 0;
 }
