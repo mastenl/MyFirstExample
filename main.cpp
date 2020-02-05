96a41059a1b2a51343dc236a525fd9aa531090e9
mastenl <31227206+mastenl@users.noreply.github.com>
2020-02-05 12:21:39 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,14 +13,3 @@ int main()
  cout << sum << endl;
  return 0;
 }
-
-nt main()
-{
- int i;
-int j;
- cout<<"Hi, please enter two numbers: ";
-   cin >> i >> j
-   int sum=i+j;
-   cout << sum << endl;
- return 0;
-}
