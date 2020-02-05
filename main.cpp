f0e2deafd811e05d6f20c91528bc68b201313b0c
mastenl <31227206+mastenl@users.noreply.github.com>
2020-02-05 12:04:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,21 @@ using std::cout;
 int main()
 {
  int i;
-int j;   
+ int j;  
+ int sum=i+j;
  cout<<"Hi, please enter two numbers: ";
-   cin >> i >> j;
-    return 0;
+ cin >> i >> j;
+ cout << sum << endl;
+ return 0;
+}
+
+nt main()
+{
+ int i;
+int j;
+ cout<<"Hi, please enter two numbers: ";
+   cin >> i >> j
+   int sum=i+j;
+   cout << sum << endl;
+ return 0;
 }
